96e5cb4845ec6321390b2895ce1466f05d521a5a
maesoko <maesoko@jupit.co.jp>
2016-05-27 18:43:15 +0900

    [modify] テーブル定義文とテーブルのデータを.txtにバックアップする処理を実装

--- backup_db.sh
+++ backup_db.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#VARIABLE
 DB_USER_NAME=
 DB_PASSWORD=
 DB_NAME=
@@ -15,6 +16,21 @@ _EOT_
  exit 1
 }
 
+function fullBackup() {
+  for database in `mysqlshow -u ${DB_USER_NAME} | awk 'NR > 3 {print $2}'`; do
+    mkdir -p db/${database}/
+    chmod 777 db/${database}/
+    backupDatabase ${database}
+  done
+}
+
+function backupDatabase() {
+  for table in `mysqlshow -u ${DB_USER_NAME} $1 | awk 'NR > 3 {print $2}'`; do
+    mysqldump -u ${DB_USER_NAME} --tab=`pwd`/db/$1/ --fields-terminated-by=, --fields-optionally-enclosed-by=\" --lines-terminated-by="\r\n" $1 ${table}
+  done
+}
+
+#argument check
 if [ $# -le 1 ] || [ $# -ge 4 ]; then
   usage
 fi
@@ -22,12 +38,16 @@ fi
 if [ $# -eq 2 ]; then
   DB_USER_NAME=$1
   DB_PASSWORD=$2
+  export MYSQL_PWD=$2
+  fullBackup
 fi
 
 if [ $# -eq 3 ]; then
   DB_USER_NAME=$1
   DB_PASSWORD=$2
   DB_NAME=$3
+  export MYSQL_PWD=$2
+  mkdir -p db/${DB_NAME}/
+  chmod 777 db/${DB_NAME}/
+  backupDatabase $DB_NAME
 fi
-
-mysqlshow -u ${DB_USER_NAME} -p${DB_PASSWORD} ${DB_NAME}
