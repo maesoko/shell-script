9f875032f6fc9c4bd334df724fa53b31902fc63c
maesoko <maesoko@jupit.co.jp>
2016-05-27 20:33:56 +0900

    [modif] 復元処理実装

--- restore_db.sh
+++ restore_db.sh
@@ -20,5 +20,25 @@ _EOT_
 #argument check
 if [ ! $# -eq 3 ]; then
   usage
+else
+  DB_USER_NAME=$1
+  DB_PASSWORD=$2
+  export MYSQL_PWD=$2
+  BACKUP_FILE=$3
+
+  #tar zxvf $BACKUP_FILE
+  for DB in `ls db`; do
+    if [[ $DB =~ .*_schema ]] || [ $DB == "sys" ] || [ $DB == "mysql" ]; then
+      continue
+    fi
+    echo $DB
+    mysql -u $DB_USER_NAME -e "CREATE DATABASE IF NOT EXISTS $DB"
+    for FILE in `ls db/$DB/*.sql`; do
+      TABLE=`basename $FILE | sed -e "s/.sql$//g"`
+      mysql -u $DB_USER_NAME $DB < $FILE
+      mysql -u $DB_USER_NAME $DB -e "LOAD DATA LOCAL INFILE 'db/$DB/$TABLE.csv' INTO TABLE $TABLE FIELDS TERMINATED BY ',' ENCLOSED BY '\"'"
+    done
+  done
 fi
+
 
